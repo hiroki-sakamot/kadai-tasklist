222f6695b1e80963b1308daf6100a80bac9b98a2
hiroki.sakamoto <sakahiro3232@gmail.com>
2020-02-20 15:39:37 +0000

    0220

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-    #before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy, :show, :edit]
+    before_action :correct_user, only: [:destroy, :show, :edit, :update]
     
     def index
         if logged_in?
