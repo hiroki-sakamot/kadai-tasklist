799b19ce50bcc8d851dc6f36b06065b6cbf9231f
hiroki.sakamoto <sakahiro3232@gmail.com>
2020-02-25 11:32:02 +0000

    0225

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,7 @@ class TasksController < ApplicationController
     before_action :correct_user, only: [:destroy, :show, :edit, :update]
     
     def index
-        if logged_in?
-          
           @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-        end
-        
     end
     
     def show
@@ -53,9 +49,7 @@ class TasksController < ApplicationController
     
     private
     
-    def set_task
-        @task = Task.find(params[:id])
-    end
+    
     
     def task_params
         params.require(:task).permit(:content, :status)
